365454717f55e98f4972aa30838e3f9283fe85e1
mkoussa <matthew.koussa@gmail.com>
2024-06-25 23:11:24 -0500

    More release prep!

--- mkoussaelixir/mix.exs
+++ mkoussaelixir/mix.exs
@@ -9,7 +9,13 @@ defmodule Mkoussaelixir.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        mkoussaelixir: [
+          include_executables_for: [:unix],
+          applications: [runtime_tools: :permanent]
+        ]
+      ]
     ]
   end
 
