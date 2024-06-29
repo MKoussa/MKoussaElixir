a187eae7c7f5a07ec7e892529ced3149b607713c
mkoussa <matthew.koussa@gmail.com>
2024-06-29 11:08:34 -0500

    pushing for gigalixir

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mkoussaelixir.MixProject do
   def project do
     [
       app: :mkoussaelixir,
-      version: "0.1.1",
+      version: "0.1.11",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
