bac8443373d746c35d93e461ca14ce693a3a48eb
mkoussa <matthew.koussa@gmail.com>
2024-10-10 13:31:36 -0500

    clean up code

--- lib/mkoussaelixir_web/live/root_live/page_live.ex
+++ lib/mkoussaelixir_web/live/root_live/page_live.ex
@@ -13,71 +13,18 @@ defmodule MkoussaelixirWeb.PageLive do
   def render(assigns) do
     ~H"""
     <div class="root-transition">
-      <%= if @current_user do %>
-        <%= if @current_user.confirmed_at do %>
-          <p style="font-family: Astloch; font-size: clamp(0.825rem, 3.6vw, 4.12rem); margin-top: clamp(-0.975rem, -4vw, -11.12rem); margin-bottom: clamp(-0.225rem, -2.6vw, -10.12rem);">
-            Public
-            <i style="font-family: 'Moirai One'; margin-right: -0.42em; font-size: clamp(1.025rem, 4.6vw, 4.12rem)">
-              F
-            </i>
-            <i style="font-family: Righteous;">eeeeeeed</i>
-          </p>
-          <section class="chat-room">
-            <div
-              id="posts"
-              style="height: calc(70vh - 10rem);
-                     overflow-y: scroll;
-                     overflow-x: hidden;
-                     padding-left: 0.2rem;
-                     padding-right: 0.2rem;
-                     border-bottom: 1rem solid var(--base-purple);
-                     animation: rainbow-border 7.5s linear;
-                     animation-iteration-count: infinite;"
-              phx-update="stream"
-            >
-              <div :for={{dom_id, post} <- @streams.posts} id={dom_id}>
-                <.live_component
-                  id={"public-post:#{post.id}"}
-                  module={MkoussaelixirWeb.RootLive.PublicFeed.Posts}
-                  liker={@current_user}
-                  repost_id={post.repost_id}
-                  show_comment_bubble={true}
-                  show_repost_bubble={true}
-                  post={post}
-                  poster_uuid={post.poster.uuid}
-                  public_profile={post.poster.public_profile}
-                  style="margin-bottom: 0.5%;"
-                />
-              </div>
-            </div>
-            <br />
-            <.live_component
-              module={MkoussaelixirWeb.RootLive.PublicFeed.Post.Form}
-              poster={@current_user}
-              new_post_form={@new_post_form}
-              id="public-post-form"
-            />
-          </section>
-        <% else %>
-          <h3>Check Your Email</h3>
-          <p>A confirmation email was sent to <i><%= @current_user.email %></i>.</p>
-          <p>
-            To unlock full account features, you'll need to validate your account by clicking the link in the email that was sent to <%= @current_user.email %>.
-          </p>
-        <% end %>
-      <% else %>
-        <h2>Welcome, <i>Friend</i></h2>
+      <h2>Welcome, <i>Friend</i></h2>
 
-        <p><i>This</i> is a <b>space</b>.</p>
+      <p><i>This</i> is a <b>space</b>.</p>
+      <span>
+        <span style="vertical-align: 1em;">Welcome to</span>
+        <h3>MKOUSSA</h3>
+      </span>
+      <p>More information in the About</p>
+      <.link href="https://weerd.space/">
         <span>
-          <span style="vertical-align: 1em;">Welcome to</span>
-          <h3>MKOUSSA</h3>
-        </span>
-        <p>More information in the About</p>
-        <.link href="https://weerd.space/">
-          <span>
-            <span style="vertical-align: 1em;">Looking for</span>
-            <span style="font-size: clamp(0.625rem, 3vw, 3.8rem);">
+          <span style="vertical-align: 1em;">Looking for</span>
+          <span style="font-size: clamp(0.625rem, 3vw, 3.8rem);">
             <span style="font-family: 'Moirai One'; font-size: clamp(0.825rem, 3.3vw, 4.12rem); margin-right: -0.42em;">
               W
             </span>
@@ -88,9 +35,8 @@ defmodule MkoussaelixirWeb.PageLive do
               Space
             </span>
           </span>?
-          </span>
-        </.link>
-      <% end %>
+        </span>
+      </.link>
     </div>
     """
   end
